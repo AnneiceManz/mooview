b67de78d8449b8c0d2bc259bc400eb581ab8e88b
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-04 15:36:33 -0600

    movie title rendering

--- client/src/components/ListStudents.jsx
+++ client/src/components/ListStudents.jsx
@@ -6,19 +6,21 @@ import Student from './Student';
 const ListStudents = () => {
 
     // this is my original state with an array of students 
-    const [students, setStudents] = useState([]);
+    const [students, setStudents] = useState(null);
 
     //this is the state needed for the UpdateRequest
     const [editingStudent, setEditingStudent] = useState(null)
 
-    const loadStudents = () => {
-        // A function to fetch the list of students that will be load anytime that list change
-        fetch("http://localhost:8080/api/movie/popular")
-            .then((response) => response.json())
-            .then((students) => {
-                setStudents(students);
-            });
-    }
+
+    async function loadStudents() {
+        // fetch the data from the backend
+        const response = await fetch(
+          "http://localhost:8080/api/movie/popular/"
+        );
+        const json = await response.json();
+        setStudents(json);
+        console.log("this is the json", json);
+      }
 
     useEffect(() => {
         loadStudents();
@@ -56,7 +58,6 @@ const ListStudents = () => {
         setEditingStudent(toUpdateStudent);
 
     }
-    console.log(students.data.results[0].original_title)
 
 
 
@@ -65,10 +66,8 @@ const ListStudents = () => {
         <div className="list-students">
             <h2>Techtonica Participants </h2>
             <ul>
-                {/* {students.map((student) => {
-                    return <li key={student.id}> <Student student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>
-                })} */}
-                <li></li>
+            {students ? students.data.results.map((student) => {return <li key={student.id}> <Student student={student} toDelete={onDelete} toUpdate={onUpdate} /></li>}) : null}
+                {/* // <li>{students ? students.data.results[0].original_title:null}</li> */}
             </ul>
         </div>
         <MyForm key={editingStudent ? editingStudent.id : null} onSaveStudent={onSaveStudent} editingStudent={editingStudent} onUpdateStudent={updateStudent} />
