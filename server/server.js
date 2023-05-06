407087fa0b2a5845f998827d33443ed347800447
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-05 18:04:14 -0600

    Created get requests for all reviews about single movie

--- server/server.js
+++ server/server.js
@@ -117,11 +117,22 @@ app.get("/api/reviews", async (req, res) => {
   }
 });
 
-// create the get request for single review in the endpoint '/api/review/:review_id'
-app.get("/api/review/:review_id", async (req, res) => {
+// create the get request for single review in the endpoint '/api/reviews/:review_id'
+app.get("/api/reviews/:review_id", async (req, res) => {
   const review_id = req.params.review_id
   try {
     const { rows: reviews } = await db.query("SELECT * FROM users WHERE review_id=$1", [review_id]);
+    res.send(reviews);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
+
+// create the get request for all reviews for single movie_id in the endpoint '/api/reviews/:movie_id'
+app.get("/api/reviews/:review_id", async (req, res) => {
+  const movie_id = req.params.movie_id
+  try {
+    const { rows: reviews } = await db.query("SELECT * FROM reviews WHERE movie_id=$1", [movie_id]);
     res.send(reviews);
   } catch (e) {
     return res.status(400).json({ e });
