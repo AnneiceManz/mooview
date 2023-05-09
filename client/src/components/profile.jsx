6b38e2a5797cce519d2c73e4db0f1d965a07a5af
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-09 15:57:12 -0600

    minor styling to profile sidebar

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Image } from "semantic-ui-react";
 
 const Profile = () => {
-
   const { user } = useAuth0();
 
   return (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <h2>{user.nickname}</h2>
-        <p>{user.email}</p>
+    <div className="profile">
+      <div className="sidebar">
+        <Image avatar size="small" src={user.picture} alt={user.name} />
+        <div className="sidebarItem">
+          <h2>{user.name}</h2>
+          <h2>{user.nickname}</h2>
+          <p>{user.email}</p>
+        </div>
+        <div className="sidebarItem">
+          <div className="sidebarButton">
+            <Button>Update Profile</Button>
+          </div>
+          <div className="sidebarButton">
+            <Button>Delete Profile</Button>
+          </div>
+        </div>
       </div>
+      <div className="profileReviews">
+        <h2>My Reviews</h2>
+      </div>
+    </div>
   );
 };
 
