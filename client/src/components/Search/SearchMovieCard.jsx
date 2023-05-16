a44bc30795dc6d226b2eb55140376ccf06b3536e
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-16 14:53:57 -0600

    styling to search cards

--- client/src/components/Search/SearchMovieCard.jsx
+++ client/src/components/Search/SearchMovieCard.jsx
@@ -1,36 +1,38 @@
 import React from "react";
-import { Icon, Image } from "semantic-ui-react";
+import { Card, Grid, Icon, Image } from "semantic-ui-react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
 const SearchMovieCard = ({ movie }) => {
   const navigate = useNavigate();
 
+  const year=movie.release_date.split("-")
+
   const handleClick = () => {
     navigate(`/movie/${movie.id}`);
   };
   return (
-    <motion.div
-      className="p-1.5 text-neutral-100 hover:bg-neautral-900 cursor-pointer"
+    <Card
       onClick={() => {
         handleClick();
       }}
     >
-      <div className="flex h-16">
         <Image
-        size="tiny"
           className="rounded aspect-[2/3]"
           src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
           alt="Couldn't find image"
         />
-        <div className="flex-col ml-3">
-          <p>{movie.title}</p>
-          <p className="text-xs text-neutral-500 line-clamp-1">
-            {movie.overview}
-          </p>
-        </div>
-      </div>
-    </motion.div>
+        <Card.Content>
+
+        <Card.Header>{movie.title}</Card.Header>
+        <Card.Meta><Icon name="star"/>{`${movie.vote_average} • ${movie.vote_count.toLocaleString()} votes • ${year[0]}`}</Card.Meta>
+        <Card.Description>
+          {movie.overview}
+        </Card.Description>
+        </Card.Content>
+
+
+    </Card>
   );
 };
 
