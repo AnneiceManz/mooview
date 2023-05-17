caf0d5349cc02a8646d081d6355ee10532f95bda
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-16 18:35:01 -0600

    Refactor search functionality to use Input component
    This commit refactors the search functionality in Searchbar.jsx to use the Input component instead of the Search component from semantic-ui-react. It also adds an useEffect hook that calls a new function, searchMovies(), which fetches data from an API endpoint and passes it to onSearch() prop. The handleSearchChange() function is updated to setQuery using e.target.value instead of {value}.

--- client/src/components/Search/Searchbar.jsx
+++ client/src/components/Search/Searchbar.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { Search } from "semantic-ui-react";
+import { Input, Search } from "semantic-ui-react";
 
-const Searchbar = () => {
+const Searchbar = ({onSearch}) => {
   const [query, setQuery] = useState("");
 
-  const handleSearchChange=(e, {value}) => {
-    setQuery(value);
+  const searchMovies = async () => {
+    if (query) {
+      try {
+        const response = await fetch("/api/movie/search/" + query);
+        const data = await response.json();
+        onSearch(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    searchMovies();
+  }, [query]);
+
+  const handleSearchChange = (e) => {
+    setQuery(e.target.value);
   }
 
 
   return (
-    <Search
+    <Input
+    icon='search'
       placeholder="Search..."
       value={query}
       onSearchChange={handleSearchChange}
