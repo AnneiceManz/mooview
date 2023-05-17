278f19d907585e841d028c6324b1e1b5b80c879a
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-16 19:56:04 -0600

    Refactor Search component to useLocation hook and fix formatting.
    This commit refactors the Search component to use the `useLocation` hook instead of `useSearchParams`. It also fixes the code formatting by removing unnecessary spaces and adding missing semicolons.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,41 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import Movie from '../components/MovieCard';
-import { useSearchParams } from 'react-router-dom';
-import { Card, Segment } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import Movie from "../components/MovieCard";
+import { useLocation } from "react-router-dom";
+import { Card, Segment } from "semantic-ui-react";
 
 const Search = () => {
+  const location = useLocation();
+  const [movies, setMovies] = useState(null);
+  const query = location.pathname.split("/")[2];
 
-    const [searchParams]=useSearchParams(); 
-
-    const [movies, setMovies] = useState(null);
-    const query = searchParams.get("query");
-
-    const getMovies = async () => {
-        try {
-            const response = await fetch(`/api/search/${query}`);
-            const json = await response.json();
-            setMovies(json)
-            console.log('here are the search results', json)
-        } catch (error) {
-            console.log('Error fetching data: ', error);
-        }
+  const getMovies = async () => {
+    try {
+      const response = await fetch(`/api/search/${query}`);
+      const json = await response.json();
+      setMovies(json);
+      console.log("here are the search results", json);
+    } catch (error) {
+      console.log("Error fetching data: ", error);
     }
+  };
 
-    useEffect(() => {
-        getMovies();
-    },[query])
+  useEffect(() => {
+    getMovies();
+  }, [query]);
 
-    return (
-        <Segment className="list__movies" style={{ overflow: "auto" }}>
-        <Card.Group itemsPerRow={6}>
-          {movies
-            ? movies.data.results.map((movie) => {
-                return <Movie key={movie.id} movie={movie} />;
-              })
-            : null}
-        </Card.Group>
-      </Segment>
-    );
+  return (
+    <Segment className="list__movies" style={{ overflow: "auto" }}>
+      <Card.Group itemsPerRow={6}>
+        {movies
+          ? movies.data.results.map((movie) => {
+              return <Movie key={movie.id} movie={movie} />;
+            })
+          : null}
+      </Card.Group>
+    </Segment>
+  );
 };
 
 export default Search;
