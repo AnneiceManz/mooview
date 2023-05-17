01e4b465d4509cecd465fabe5c0d28dfc3daaa5f
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-17 15:46:34 -0600

    Refactor CommentForm and add CommentForm component to Reviews
    - Refactored CommentForm component to use react-router-dom's useLocation hook
    - Added a new state variable writeComment to store user input in the form
    - Added onSubmitForm function that sends POST request with writeComment data to /api/comments endpoint
    - Added getComments function that retrieves comments for a specific review from /api/comments/:review_id endpoint
    - Added useEffect hook that calls getComments when review_id changes
    - Updated return statement of CommentForm component to display comments and a form for adding new comments
    - Imported CommentForm into Reviews component and passed review_id and user props

--- client/src/components/CommentForm.jsx
+++ client/src/components/CommentForm.jsx
@@ -1,74 +1,81 @@
-import React, {useEffect, useState} from 'react';
-import { Comment, Header, Form, Button } from 'semantic-ui-react';
+import { useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect, useState } from "react";
+import { Comment, Header, Form, Button } from "semantic-ui-react";
+import { useLocation } from "react-router-dom";
 
-const CommentForm = ({review_id, user}) => {
-
-    const userId = user
+const CommentForm = ({ review_id, user }) => {
+ const reviewId= review_id
+    console.log('this is the review id', reviewId)
     
+    const state = useLocation().state;
+  const [writeComment, setWriteComment] = useState(
+    state || {
+      user_id: user,
+      review_id: review_id,
+      comment_text: "",
+    }
+  );
 
-    const [writeComment, setWriteComment] = useState(
-        state || {
-            user_id: userId,
-            review_id: review_id,
-            comment_text: "",
-        }
-      )
+  const [comments, setComments] = useState(null);
 
-      const [comments, setComments] = useState([])
-    
-      const handleChange = (e) => {
-        setWriteComment ({ ...writeComment, [e.target.name]: e.target.value})
-      }
+  const handleChange = (e) => {
+    setWriteComment({ ...writeComment, [e.target.name]: e.target.value });
+  };
 
-    const onSubmitForm = async (e) => {
-        e.preventDefault();
-        try {
-          const response = await fetch("/api/comments", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(writeComment),
-          });
-          window.location.reload();
-        } catch (error) {
-          console.log(error.message);
-        }
-      };  
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/comments", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(writeComment),
+      });
+      window.location.reload();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-      const getComments = async () => {
-        try {
-           const response = await fetch(`/api/comments/${review_id}`);
-            const comments = await response.json();
-            setComments(comments);
-        } catch (error) {
-            console.error(error.message);
-        }
-      }
+  const getComments = async () => {
+    try {
+      const response = await fetch(`/api/comments/${review_id}`);
+      const comments = await response.json();
+      setComments(comments);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-      useEffect(() => {
-        getComments();
-      },[review_id])
+  useEffect(() => {
+    getComments();
+  }, [review_id]);
 
-    return (
-        <Comment.Group>
-            <Header as='h3' dividing>
-                Comments
-            </Header>
-            {comments.map((comment) => (
-                <Comment>
-                    <Comment.Content>
-                        <Comment.Metadata>Posted: {comment.time_stamp}</Comment.Metadata>
-                        <Comment.Text>
-                            <p>{comment.comment_text}</p>
-                        </Comment.Text>
-                    </Comment.Content>
-                </Comment>
-            ))}
-            <Form reply onSubmit={onSubmitForm}>
-                <Form.TextArea name="comment_text" onChange={handleChange} />
-                <Button content='Add Comment' labelPosition='left' icon='edit' primary />
-            </Form>
-        </Comment.Group>
-    );
+  return (
+    <Comment.Group>
+      <Header as="h3" dividing>
+        Comments
+      </Header>
+      {comments ? comments.map((comment) => (
+        <Comment>
+          <Comment.Content>
+            <Comment.Metadata>Posted: {comment.time_stamp}</Comment.Metadata>
+            <Comment.Text>
+              <p>{comment.comment_text}</p>
+            </Comment.Text>
+          </Comment.Content>
+        </Comment>
+      ) ) : null}
+      <Form reply onSubmit={onSubmitForm}>
+        <Form.TextArea name="comment_text" onChange={handleChange} />
+        <Button
+          content="Add Comment"
+          labelPosition="left"
+          icon="edit"
+          primary
+        />
+      </Form>
+    </Comment.Group>
+  );
 };
 
 export default CommentForm;
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Card, Button, Icon } from "semantic-ui-react";
 import UpdateReview from "./UpdateReview";
+import CommentForm from "./CommentForm";
 
 const Reviews = ({ review, movieName }) => {
   const { user } = useAuth0();
@@ -23,6 +24,7 @@ const Reviews = ({ review, movieName }) => {
           </Button.Group>
         </Card.Content>
       )}
+      <CommentForm review_id={review.review_id} user={user?.sub} />
     </Card>
   );
 };
