960c43baafaa17bd6b765edccab2858f93e9264b
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-16 19:39:39 -0600

    Add useState and semantic-ui-react to Search page
    This commit adds the useState hook and imports the Card and Segment components from semantic-ui-react to the Search page.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Movie from '../components/MovieCard';
 import { useSearchParams } from 'react-router-dom';
+import { Card, Segment } from 'semantic-ui-react';
 
 const Search = () => {
 
