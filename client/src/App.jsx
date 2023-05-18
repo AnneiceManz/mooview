2d55392c2978756d763710c025199ad2fbc29026
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-18 12:45:16 -0600

    Add AuthGuard to protect profile route
    This commit adds an AuthGuard component to the profile route in App.jsx. This will ensure that only authenticated users can access the profile page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MyNavBar from "./components/Navbar";
 import Search from "./pages/Search";
+import { AuthGuard } from "./components/Auth0/AuthGuard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MyNavBar />}>
         <Route index element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<AuthGuard component={Profile }/>} />
         <Route path="/movie/:movie_id" element={<SingleMovie />} />
         <Route path="/search/:search_term" element={<Search />} />
       </Route>
