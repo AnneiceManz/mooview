c62bc81c2eb58240a1b7dfe638bc4edde42ee2ba
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-17 19:06:49 -0600

    Add multiple images to IMAGES.jsx
    This commit adds two new images to the IMAGES.jsx file: mooview_logo2, and mooview_logo3.

--- client/src/images/IMAGES.jsx
+++ client/src/images/IMAGES.jsx
@@ -1,5 +1,7 @@
 const IMAGES = {
-    mooview_logo : new URL ('./Mooviewlogo.png', import.meta.url).href
+    mooview_logo : new URL ('./Mooviewlogo.png', import.meta.url).href,
+    mooview_logo2 : new URL ('./1.png', import.meta.url).href,
+    mooview_logo3 : new URL ('./2.png', import.meta.url).href,
 }
 
 export default IMAGES
