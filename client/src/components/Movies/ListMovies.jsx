5453c1ea0ef49d249e865cd52b708060b214822f
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-19 17:54:04 -0600

    Refactor ListMovies component to remove unused imports and comments
    This commit removes unused imports and comments from the ListMovies component. The code is now cleaner and easier to read.

--- client/src/components/Movies/ListMovies.jsx
+++ client/src/components/Movies/ListMovies.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import Movie from "./MovieCard";
-import { Card, Segment } from "semantic-ui-react";
 import {motion} from "framer-motion";
 
 const ListMovies = () => {
-  // this is my original state with an array of students
   const [movies, setMovies] = useState(null);
   const [width, setWidth] = useState(0);
   const carouselRef = useRef();
 
   async function loadMovies() {
-    // fetch the data from the backend
     const response = await fetch("/api/movie/popular/");
     const json = await response.json();
     setMovies(json);
