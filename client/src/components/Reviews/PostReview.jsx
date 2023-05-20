dcd238601a7c7e39512f909f2fd6aaed2bc4538c
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-19 18:02:13 -0600

    Cleaned up reviews code

--- client/src/components/Reviews/PostReview.jsx
+++ client/src/components/Reviews/PostReview.jsx
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
 import { Button, Form, Modal, Rating } from "semantic-ui-react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const PostReview = ({ user, movie_id, movieName }) => {
   const [showModal, setShowModal] = useState(false);
-  const [rating, setRating] = useState(0)
-  const state = useLocation().state
-  const userId = user
-  const movieId = parseInt(movie_id.replace('/movie/', ''));
-  const navigate = useNavigate()
+  const [rating, setRating] = useState(0);
+  const state = useLocation().state;
+  const userId = user;
+  const movieId = parseInt(movie_id.replace("/movie/", ""));
 
   const [writeReview, setWriteReview] = useState(
     state || {
-        user_id: userId,
-        movie_id: movieId,
-        title: "",
-        post: "",
-        star_rating: 0
+      user_id: userId,
+      movie_id: movieId,
+      title: "",
+      post: "",
+      star_rating: 0,
     }
-  )
+  );
 
   const handleChange = (e) => {
-    setWriteReview ({ ...writeReview, [e.target.name]: e.target.value})
-  }
-  console.log(writeReview.star_rating, 'stars!!!')
+    setWriteReview({ ...writeReview, [e.target.name]: e.target.value });
+  };
+  console.log(writeReview.star_rating, "stars!!!");
 
-  const handleRate = (e, {rating}) => {
-    setRating(rating)
+  const handleRate = (e, { rating }) => {
+    setRating(rating);
     setWriteReview({
-        ...writeReview, star_rating: rating
-    })
-    console.log(rating, 'stars')
-}
+      ...writeReview,
+      star_rating: rating,
+    });
+    console.log(rating, "stars");
+  };
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
@@ -48,57 +48,58 @@ const PostReview = ({ user, movie_id, movieName }) => {
   };
 
   return (
-
-      <Modal
-        centered
-        onClose={() => setShowModal(false)}
-        onOpen={() => setShowModal(true)}
-        open={showModal}
-        trigger={<Button size="small" color="blue">Post Review</Button>}
-        size="small"
-        style={{ height: "auto", margin: "20%" }}
-      >
-        <Modal.Header>Write a Review for {movieName}</Modal.Header>
-        <Modal.Content>
-          <Form
-          id='postSubmission'
+    <Modal
+      centered
+      onClose={() => setShowModal(false)}
+      onOpen={() => setShowModal(true)}
+      open={showModal}
+      trigger={
+        <Button size="small" color="blue">
+          Post Review
+        </Button>
+      }
+      size="small"
+      style={{ height: "auto", margin: "20%" }}
+    >
+      <Modal.Header>Write a Review for {movieName}</Modal.Header>
+      <Modal.Content>
+        <Form
+          id="postSubmission"
           action="#postSubmission"
           onSubmit={onSubmitForm}
-          >
-            <span>Rating: {rating} stars </span>
-            <Rating
-             icon="star"
-             size="huge" 
-             maxRating={10}
-             name="star_rating"
-             onRate={handleRate} 
-             />
-            <Form.Input
-              fluid
-              placeholder="Title"
-              name="title"
-              value={writeReview.title}
-              onChange={handleChange}
-            />
-            <Form.TextArea 
+        >
+          <span>Rating: {rating} stars </span>
+          <Rating
+            icon="star"
+            size="huge"
+            maxRating={10}
+            name="star_rating"
+            onRate={handleRate}
+          />
+          <Form.Input
+            fluid
+            placeholder="Title"
+            name="title"
+            value={writeReview.title}
+            onChange={handleChange}
+          />
+          <Form.TextArea
             placeholder="Review Post"
-              name="post"
-              value={writeReview.post}
-              onChange={handleChange}
-            />
-            <Button.Group>
-              <Button 
-              color="blue"
-              id="submitPost"
-              >Post</Button>
-              <Button color="red" onClick={() => setShowModal(false)}>
-                Cancel
-              </Button>
-            </Button.Group>
-          </Form>
-        </Modal.Content>
-      </Modal>
-
+            name="post"
+            value={writeReview.post}
+            onChange={handleChange}
+          />
+          <Button.Group>
+            <Button color="blue" id="submitPost">
+              Post
+            </Button>
+            <Button color="red" onClick={() => setShowModal(false)}>
+              Cancel
+            </Button>
+          </Button.Group>
+        </Form>
+      </Modal.Content>
+    </Modal>
   );
 };
 
