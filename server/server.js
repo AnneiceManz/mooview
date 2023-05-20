17fd5664793393388db5de8a2f399b716842332e
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-19 21:40:39 -0600

    Refactor family movie endpoint to use async/await and axios
    This commit refactors the family movie endpoint to use async/await and axios instead of fetch. It also adds error handling in case of an error.

--- server/server.js
+++ server/server.js
@@ -423,19 +423,18 @@ app.get("/api/movie/animated/", async (req, res) => {
 });
 
 //creates endpoint to fetch family movies
-app.get("/api/movie/family/", (req, res) => {
-  const api_key = process.env.API_KEY;
+app.get("/api/movie/family/", async (req, res) => {
+  try {
+    const apiKey = process.env.API_KEY;
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&include_adult=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=10751`;
 
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&include_adult=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=10751`;
-
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      res.send({ data });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    const response = await axios.get(url);
+    const data = response.data;
+    res.send({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 //creates endpoint to fetch scifi movies
