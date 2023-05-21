e3381b0668bacec33882e090574f37c61144cc86
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-20 22:03:48 -0600

    Refactor MovieCard component to improve performance and add hover effect
    This commit removes the framer-motion library, simplifies the Card component by removing Image, and adds a new hover effect with an overlay. The changes also include updating the CSS classes for responsive design.

--- client/src/components/Movies/MovieCard.jsx
+++ client/src/components/Movies/MovieCard.jsx
@@ -1,7 +1,6 @@
-import { motion } from "framer-motion";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Icon} from "semantic-ui-react";
 
 const Movie = ({ movie }) => {
   const navigate = useNavigate();
@@ -11,19 +10,19 @@ const Movie = ({ movie }) => {
   };
 
   return (
-    <div className="item">
-      <Card centered>
-        <Image
-          centered
-          size="medium"
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          onClick={handleClick}
-        />
-        <div className="overlay-icon">
-          {movie.vote_average} <Icon name="star" color="yellow" />
-          's
+    <div className="w-[180px] sm:w-[250px] md:w-[320px] lg:w-[320px] xl:w-[280px] inline-block cursor-pointer relative p-4 z-0">
+      <img
+        className="w-full h-auto hover:opacity-50 rounded"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={`${movie.title} Poster`}
+        onClick={handleClick}
+      />
+      <div className="absolute top-0 left-0 w-full hover:bg-black/40 opacity-0 hover:opacity-100 text-white px-5">
+        <div className="flex flex-row justify-center items-center absolute top-8 right-8 text-gray-300">
+          <Icon name="star" />
+          <p className="text-xs md:text-base font-bold">{movie.vote_average}</p>
         </div>
-      </Card>
+      </div>
     </div>
   );
 };
