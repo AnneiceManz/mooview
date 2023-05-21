1894a6c4cd387b4fbe81e79325d7f9bfc67da06c
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-20 23:35:07 -0600

    Refactor movie list components to use font awesome icons for slider buttons.
    This commit refactors the List_Now_Playing and ListAction components to use Font Awesome icons instead of custom SVGs for slider buttons. It also removes unused code from List_Now_Playing.

--- client/src/components/Movies/List_Now_Playing.jsx
+++ client/src/components/Movies/List_Now_Playing.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Movie from "./MovieCard";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ListNowPlaying = () => {
   const [movies, setMovies] = useState(null);
-
 
   async function loadMovies() {
     // fetch the data from the backend
@@ -22,30 +24,43 @@ const ListNowPlaying = () => {
   const slideLeft = () => {
     let slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft - 500;
-  }
+  };
 
   const slideRight = () => {
     let slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft + 500;
-  }
+  };
 
   return (
-  <div>
-    <div className="flex flex-row items-center">
-      <h2 className="text-[#FFFDE3] font-bold md:text-xl p-4 cursor-pointer">Now Playing</h2>
-    </div>
-    <div className="relative flex items-center ml-2">
-      <FontAwesomeIcon icon={faCircleChevronLeft} className="text-white rounded-full left-0 absolute opacity-80 hover:opacity-100 cursor-pointer z-10  hover:block text-4xl" onClick={slideLeft}/>
-      <div id="slider" className="w-full h-full overflow-x-scroll touch-auto hover:scroll-auto whitespace-nowrap scroll-smooth scrollbar-hide relative break-words">
-      {movies
-              ? movies.data.results.map((movie) => {
-                  return <Movie key={movie.id} movie={movie} />;
-                })
-              : null}
+    <div>
+      <div className="flex flex-row items-center">
+        <h2 className="text-black font-bold md:text-3xl p-4 cursor-pointer">
+          Now Playing
+        </h2>
       </div>
-      <FontAwesomeIcon icon={faCircleChevronRight} className="text-white rounded-full right-0 absolute opacity-80 hover:opacity-100 cursor-pointer z-10  hover:block text-4xl" onClick={slideRight}/>
+      <div className="relative flex items-center ml-2">
+        <FontAwesomeIcon
+          icon={faCircleChevronLeft}
+          className="text-white rounded-full left-0 absolute opacity-80 hover:opacity-100 cursor-pointer z-10  hover:block text-4xl"
+          onClick={slideLeft}
+        />
+        <div
+          id="slider"
+          className="w-full h-full overflow-x-scroll touch-auto hover:scroll-auto whitespace-nowrap scroll-smooth scrollbar-hide relative break-words"
+        >
+          {movies
+            ? movies.data.results.map((movie) => {
+                return <Movie key={movie.id} movie={movie} />;
+              })
+            : null}
+        </div>
+        <FontAwesomeIcon
+          icon={faCircleChevronRight}
+          className="text-white rounded-full right-0 absolute opacity-80 hover:opacity-100 cursor-pointer z-10  hover:block text-4xl"
+          onClick={slideRight}
+        />
+      </div>
     </div>
-  </div>
   );
 };
 
