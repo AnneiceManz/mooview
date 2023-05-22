fa37e14d9c8b67766fe473bbcb200d1dec2f2c76
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-22 10:19:02 -0600

    Refactor write review section in Reviews component
    This commit refactors the write review section in the Reviews component by removing unnecessary code and reordering elements for better user experience. The new design includes a rating system, title input field, post text area, and a submit button. If the user is not authenticated, they are prompted to log in before posting a review.

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,20 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React, { useState, useEffect } from "react";
 import {
-  Card,
   Button,
-  Icon,
-  Image,
   Confirm,
-  Form,
   Rating,
 } from "semantic-ui-react";
 import UpdateReview from "./UpdateReview";
 import CommentForm from "../Comments/CommentForm";
 import { useLocation } from "react-router-dom";
 import LoginText from "../Auth0/LoginText";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const Reviews = ({ movieName, movie_id }) => {
   const { user, isAuthenticated } = useAuth0();
@@ -97,8 +91,82 @@ const Reviews = ({ movieName, movie_id }) => {
   return (
     <div className="my-10 relative">
       <div className="mt-10 md:grid flex-col grid-cols-5 gap-4 bg-dry xs:p-10 py-10 px-2 sm:p-20 rounded">
+        {/* REVIWERS */}
+        <div className="col-span-3 flex flex-col gap-6">
+          <h3 className="text-xl text-text font-semibold">Reviews </h3>
+          <div className="w-full flex flex-col bg-main gap-6 rounded-lg md:p-12 p-6 h-header overflow-y-scroll">
+            {reviews
+              ? reviews.map((review) => {
+                  return (
+                    <div className="md:grid flex flex-col w-full grid-cols-10 gap-6 bg-dry p-4 border border-gray-100 shadow-xl rounded-lg">
+                      <div className="col-span-10 flex flex-col gap-2">
+                        <div className="grid flex flex-col grid-clos-4 lg:grid-cols-8 lg:grid-rows-3 gap-5 h-[38%] place-items-center">
+                          <div className=" col-span-1 lg:col-span-2 lg:row-span-2  m-auto">
+                            <img
+                              src={review.picture}
+                              alt={review.username}
+                              className="w-[80px] lg:w-[104px] h-auto rounded-full hidden md:block"
+                            />
+                            <p className="text-slate-500 text-lg row-span-1">
+                              {review.username}
+                            </p>
+                          </div>
+
+                          <div className="col-span-2 lg:col-span-3 lg:row-span-3 text-center ">
+                            <p className="hidden lg:block lg:text-3xl font-bold tracking-wider">
+                              {review.title}
+                            </p>
+                          </div>
+                          <div className=" col-span-2 lg:col-span-3 lg:row-span-3 lg:text-4xl text-right m-4">
+                            <Rating
+                              icon="star"
+                              color="yellow"
+                              disabled
+                              maxRating={10}
+                              rating={review.star_rating}
+                            />
+                          </div>
+                        </div>
+                        <p className="text-xl leading-6 font-medium">
+                          {review.post}
+                        </p>
+                      </div>
+                      {/* rates */}
+                      <div>
+                        {currentUser === review.user_id && (
+                          <Button.Group size="tiny">
+                            <UpdateReview
+                              review={review}
+                              movieName={movieName}
+                            />
+                            <Button
+                              color="red"
+                              onClick={() => setConfirm(true)}
+                            >
+                              Delete
+                            </Button>
+                            <Confirm
+                              cancelButton="Never mind"
+                              confirmButton="Delete Review"
+                              content="Are you sure you want to delete this review?"
+                              open={confirm}
+                              onCancel={() => setConfirm(false)}
+                              onConfirm={handleDelete}
+                            />
+                          </Button.Group>
+                        )}
+                      </div>
+                      <div className="col-span-10 flex flex-col gap-2">
+                        <CommentForm review_id={review.review_id} />
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+        </div>
         {/* write review */}
-        <div className="md:col-span-2 w-full flex flex-col gap-8 ">
+        <div className="md:col-span-2 w-full flex flex-col gap-8 order-first ">
           <h3 className="text-xl text-text font-semibold">
             Review "{movieName}"
           </h3>
@@ -153,97 +221,16 @@ const Reviews = ({ movieName, movie_id }) => {
             />
             {!isAuthenticated && (
               <>
-              <span>
-                <LoginText /> to post a review!
-              </span>
-            </>
-          )}
-          {isAuthenticated && (
-            <Button.Group>
-
-              <Button color="blue" id="submitPost" onClick={onSubmitForm}>
-                Post
-              </Button>
-              <Button color="red" onClick={() => setShowModal(false)}>
-                Cancel
-              </Button>
-            </Button.Group>
-          )}
-          </div>
-        </div>
-        {/* REVIWERS */}
-        <div className="col-span-3 flex flex-col gap-6">
-          <h3 className="text-xl text-text font-semibold">Reviews </h3>
-          <div className="w-full flex flex-col bg-main gap-6 rounded-lg md:p-12 p-6 h-header overflow-y-scroll">
-            {reviews
-              ? reviews.map((review) => {
-                  return (
-                    <div className="md:grid flex flex-col w-full grid-cols-10 gap-6 bg-dry p-4 border border-gray-100 shadow-xl rounded-lg">
-                      <div className="col-span-10 flex flex-col gap-2">
-                        <div className="grid flex flex-col grid-clos-4 lg:grid-cols-8 lg:grid-rows-3 gap-5 h-[38%] place-items-center">
-                          <div className=" col-span-1 lg:col-span-2 lg:row-span-2  m-auto">
-                            <img
-                              src={review.picture}
-                              alt={review.username}
-                              className="w-[80px] lg:w-[104px] h-auto rounded-full hidden md:block"
-                            />
-                            <p className="text-slate-500 text-lg row-span-1">
-                              {review.username}
-                            </p>
-                          </div>
-
-                          <div className="col-span-2 lg:col-span-3 lg:row-span-3 text-center ">
-                            <p className="hidden lg:block lg:text-3xl font-bold tracking-wider">
-                              {review.title}
-                            </p>
-                          </div>
-                          <div className=" col-span-2 lg:col-span-3 lg:row-span-3 lg:text-4xl text-right m-4">
-                            <Rating
-                     
-                              icon="star"
-                              color="yellow"
-                              disabled
-                              maxRating={10}
-                              rating={review.star_rating}
-                            />
-                          </div>
-                        </div>
-                        <p className="text-xl leading-6 font-medium">
-                          {review.post}
-                        </p>
-                      </div>
-                      {/* rates */}
-                      <div>
-                        {currentUser === review.user_id && (
-                          <Button.Group size="tiny">
-                            <UpdateReview
-                              review={review}
-                              movieName={movieName}
-                            />
-                            <Button
-                              color="red"
-                              onClick={() => setConfirm(true)}
-                            >
-                              Delete
-                            </Button>
-                            <Confirm
-                              cancelButton="Never mind"
-                              confirmButton="Delete Review"
-                              content="Are you sure you want to delete this review?"
-                              open={confirm}
-                              onCancel={() => setConfirm(false)}
-                              onConfirm={handleDelete}
-                            />
-                          </Button.Group>
-                        )}
-                      </div>
-                          <div className="col-span-10 flex flex-col gap-2">
-                      <CommentForm review_id={review.review_id} />
-                      </div>
-                    </div>
-                  );
-                })
-              : null}
+                <span>
+                  <LoginText /> to post a review!
+                </span>
+              </>
+            )}
+            {isAuthenticated && (
+                <Button color="blue" id="submitPost" onClick={onSubmitForm}>
+                  Post
+                </Button>
+            )}
           </div>
         </div>
       </div>
@@ -252,58 +239,3 @@ const Reviews = ({ movieName, movie_id }) => {
 };
 
 export default Reviews;
-{
-  /* // <>
-  //   <Card centered>
-  //     <Card.Content>
-  //       <div>
-
-  //       <Image 
-  //       floated="left"
-  //       circular
-  //       size="tiny"
-  //       src={review.picture}
-  //       />
-  //       </div>
-  //       <Card.Header style={{fontSize: '1.8em'}}>{review.title}</Card.Header>
-  //       <Card.Meta
-  //         style={{ color: "red", fontWeight: "bold" }}
-  //       >
-  //         <span style={{ color: "#3977C9", marginLeft:'2px' }}>{review.username}</span>
-  //       </Card.Meta>
-  //       <Card.Meta>
-  //         {review.star_rating} <Icon name="star" color="yellow" />
-  //         's for {movieName}!
-  //       </Card.Meta>
-  //       <Card.Description><p className="pt-5">{review.post}</p></Card.Description>
-  //     </Card.Content>
-  //     {currentUser === review.user_id && (
-  //       <Card.Content extra>
-  //         <Button.Group size="tiny">
-  //           <UpdateReview review={review} movieName={movieName} />
-  //           <Button color="red" onClick={()=>setConfirm(true)}>
-  //             Delete
-  //           </Button>
-  //           <Confirm 
-  //           cancelButton="Never mind"
-  //           confirmButton="Delete Review"
-  //           content="Are you sure you want to delete this review?"
-  //           open={confirm}
-  //           onCancel={() => setConfirm(false)}
-  //           onConfirm={handleDelete}
-  //           />
-  //         </Button.Group>
-  //       </Card.Content>
-  //     )}
-  //   </Card>
-  //   <CommentForm review_id={review.review_id} />
-  // </>
-
-
-
-
-
-
-
-    */
-}
