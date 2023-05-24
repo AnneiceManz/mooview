cbf480879134fdd4990bc3abeeeedcb66a1962e6
Anneice Manzanares <mountains.stars@gmail.com>
2023-05-24 12:06:04 -0600

    Add data-testid to Main component and add test for Main component
    This commit adds a `data-testid` attribute to the `Main` component in `Main.jsx`. It also adds a new test case in `example.test.jsx` that checks if the `Main` component renders correctly by using the newly added `data-testid`.

--- client/src/example.test.jsx
+++ client/src/example.test.jsx
@@ -4,6 +4,7 @@ import {expect, test} from 'vitest';
 import {render, screen} from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Footer from './components/PageElements/Footer';
+import Main from './components/PageElements/Main';
 
 
 test('Navbar renders correctly', () => {
@@ -22,5 +23,16 @@ test('Footer renders correctly', () => {
   </Router>);
   const footerElement = getByTestId('footer');
   expect(footerElement).toBeDefined();
+});
+
+// test if main renders correctly
+
+test('Footer renders correctly', () => {
+  const { getByTestId } = render(
+  <Router>
+  <Main />
+  </Router>);
+  const mainElement = getByTestId('main');
+  expect(mainElement).toBeDefined();
 });
 
